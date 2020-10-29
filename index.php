3844ce1648f1fdd7e5385bc5b507c5d07d450a02
PiotrPistelok <63048870+PiotrPistelok@users.noreply.github.com>
2020-10-29 15:04:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
-    echo("Hello world");
+$servername = "remotemysql.com";
+$username ="fv01d6C7kM";
+$password = "AfwVdCmSd5";
+$dbname = "fv01d6C7kM";
+
+$conn= new mysqli($servername,$username,$password,$dbname);
+
+$sql ="select * from pracownicy";
+echo("<h3>ZAD.1</h3>");
+$result = mysqli_query($conn, $sql);
+
+echo('<table border="1" class="tabela"');
+echo ("<tr><th>id</th><th>imie</th><th>dzial</th><th>zarobki</th></tr>");
+while($row=mysqli_fetch_assoc($result)){
+echo("<tr>");
+    //print_r($row);
+    echo("<tr>");
+    echo("<td>".$row['id']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
+    echo("<tr>");
+echo("</tr>");
+
+}
+
 ?>
