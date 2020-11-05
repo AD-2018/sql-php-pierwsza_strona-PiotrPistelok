007aa1b2184a75931bb064feb1d2b7823cd6178b
PiotrPistelok <63048870+PiotrPistelok@users.noreply.github.com>
2020-11-05 14:40:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,15 +16,15 @@
 require_once("connect.php");
 
 echo("<br>Zad 1 <br>");
-$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial";
-echo($sql);
+//$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial";
+//echo($sql);
 
-$result = mysqli_query($conn, $sql);
-if ( $result) {
-        echo "<li>ok";
-    } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
+//$result = mysqli_query($conn, $sql);
+//if ( $result) {
+//        echo "<li>ok";
+//    } else {
+//      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+//    }
 
 echo('<table border="1">');
     echo('<th>Imie</th><th>zarobki</th><th>nazwa dzialu</th>');
