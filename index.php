d7dea7ea6cc6a1d26aaca7c56c99819ea6f92bcd
Tomjand <domagala.tomasz@gmail.com>
2020-11-05 13:59:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,27 +7,27 @@
 <link href="https://github.com/AD-2018/sql-php-pierwsza_strona-PiotrPistelok">github</a>
     <h1>Piotr Pistelok nr 9</h1>
 <?php
-include 'connect.php';
-echo("<h3>ZAD.1</h3>");
-$sql ="select * from pracownicy";
-    echo ("<li>".$sql):
-$result = mysqli_query($conn, $sql);
-    if ($conn->query($sql)) {
-        echo "ok";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-}
+// include 'connect.php';
+// echo("<h3>ZAD.1</h3>");
+// $sql ="select * from pracownicy";
+//     echo ("<li>".$sql):
+// $result = mysqli_query($conn, $sql);
+//     if ($conn->query($sql)) {
+//         echo "ok";
+//     } else {
+//         echo "Error: " . $sql . "<br>" . $conn->error;
+// }
 
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id</th><th>imie</th><th>dzial</th><th>zarobki</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-    echo("<tr>");
-    //print_r($row);
-        echo("<tr>");
-        echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
-        echo("<tr>");
-    echo("</tr>");
-}
+// echo('<table border="1" class="tabela"');
+// echo ("<tr><th>id</th><th>imie</th><th>dzial</th><th>zarobki</th></tr>");
+// while($row=mysqli_fetch_assoc($result)){
+//     echo("<tr>");
+//     //print_r($row);
+//         echo("<tr>");
+//         echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
+//         echo("<tr>");
+//     echo("</tr>");
+// }
 ?>
 </body>
 </html>
