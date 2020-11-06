7642f9b7b26e8a9dfbd21b4820e70ae3511bc9a7
PiotrPistelok <63048870+PiotrPistelok@users.noreply.github.com>
2020-11-06 11:55:46 +0100

    Update agreg.php

--- agreg.php
+++ agreg.php
@@ -15,7 +15,7 @@
 
 <?php
 require_once("connect.php");
-echo("<br>Zad 1 <br>");
+echo("<br>Suma zarobków wszystkich pracowników<br>");
 $sql = "SELECT sum(zarobki) as suma_zarobków FROM pracownicy, organizacja where id_org=dzial;";
 echo($sql);
 
@@ -37,8 +37,8 @@ echo('<table border="1">');
 
     echo('</table>');
 
-echo("<br>Zad 2 <br>");
-$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial and imie like '%a'";
+echo("<br>Suma zarobków wszystkich kobiet<br>");
+$sql = "SELECT sum(zarobki) as suma_zarobków FROM pracownicy, organizacja where id_org=dzial and imie like '%a'";
 echo($sql);
 
 $result = mysqli_query($conn, $sql);
@@ -53,13 +53,13 @@ echo('<table border="1">');
 
     while($row=mysqli_fetch_assoc($result)){
         echo('<tr>');
-        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['nazwa_dzial'].'</td>');
+        echo('<td>'.$row['suma_zarobkow'].'</td>');
         echo('</tr>');
     }
 
     echo('</table>');
-echo("<br>Zad 3 <br>");
-$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial and imie not like '%a'";
+echo("<br>Suma zarobków mężczyzn pracujących w dziale 2 i 3<br>");
+$sql = "SELECT sum(zarobki) as suma_zarobków FROM pracownicy, organizacja where id_org=dzial and dzial in (2,3) and imie not like '%a'";
 echo($sql);
 
 $result = mysqli_query($conn, $sql);
@@ -74,7 +74,7 @@ echo('<table border="1">');
 
     while($row=mysqli_fetch_assoc($result)){
         echo('<tr>');
-        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['nazwa_dzial'].'</td>');
+        echo('<td>'.$row['suma_zarobków'].'</td>');
         echo('</tr>');
     }
 
