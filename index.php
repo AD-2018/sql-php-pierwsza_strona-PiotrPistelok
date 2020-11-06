df6422e9b13981eb434c036629807d7b9ae103a4
PiotrPistelok <63048870+PiotrPistelok@users.noreply.github.com>
2020-11-06 12:17:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-PiotrPistelok">GitHub</a>
     <h2>Piotr Pistelok nr 9</h2>
 <div class="nav">
+    <a href="Organizacja.php">Organizacja</a>
     <a href="OrgPracownicy.php">Organizacja Pracownicy</a>
     <a href="agreg.php">Funkcje Agregujące</a>
 </div>
