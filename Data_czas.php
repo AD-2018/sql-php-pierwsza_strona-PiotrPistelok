34b6ae282e47b8ee3a0c48520c6423f497dd6d68
PiotrPistelok <63048870+PiotrPistelok@users.noreply.github.com>
2020-11-12 14:37:22 +0100

    Update Data_czas.php

--- Data_czas.php
+++ Data_czas.php
@@ -29,10 +29,153 @@ if ( $result) {
     }
 echo('<table border="1">');
     echo('<th>Imie</th><th>Dział</th><th>Wiek</th>');
-
     while($row=mysqli_fetch_assoc($result)){
         echo('<tr>');
         echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['dzial'].'</td><td>'.$row['wiek'].'</td>');
+        echo('</tr>');
+    }
+    echo('</table>');
+  
+  echo("<br>Wiek poszczególnych pracowników (w latach) z działu serwis<br>");
+$sql ="select *,YEAR(curdate())-YEAR(data_urodzenia) AS wiek from pracownicy,organizacja where id_org=dzial and dzial=1";
+echo($sql);
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+echo('<table border="1">');
+    echo('<th>Imie</th><th>Dział</th><th>Wiek</th>');
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['dzial'].'</td><td>'.$row['wiek'].'</td>');
+        echo('</tr>');
+    }
+    echo('</table>');
+  
+  echo("<br>Suma lat wszystkich pracowników<br>");
+$sql ="select *,SUM(YEAR(CURDATE()) - YEAR(data_urodzenia)) as Suma from pracownicy,organizacja where id_org=dzial"; 
+echo($sql);
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+echo('<table border="1">');
+    echo('<th>Suma Lat</th>');
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo("<td>".$row['Suma']."</td>");     
+        echo('</tr>');
+    }
+    echo('</table>');
+  
+  echo("<br>Suma lat pracowników z działu handel<br>");
+$sql ="select *,SUM(YEAR(CURDATE()) - YEAR(data_urodzenia)) as Suma from pracownicy,organizacja where id_org=dzial and dzial=2";
+echo($sql);
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+echo('<table border="1">');
+    echo('<th>Suma lat');
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo("<td>".$row['Suma']."</td>");     
+        echo('</tr>');
+    }
+    echo('</table>');
+  
+  echo("<br>Suma lat kobiet<br>");
+    $sql ="select *,SUM(YEAR(CURDATE()) - YEAR(data_urodzenia)) as Suma from pracownicy,organizacja where id_org=dzial and imie like'%a'"; 
+echo($sql);
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+echo('<table border="1">');
+    echo('<th>Suma lat</th>');
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo("<td>".$row['Suma']."</td>");
+        echo('</tr>');
+    }
+    echo('</table>');
+  
+    echo("<br>Suma lat mężczyzn<br>");
+$sql ="select *,SUM(YEAR(CURDATE()) - YEAR(data_urodzenia)) as Suma from pracownicy,organizacja where id_org=dzial and imie not like'%a'"; 
+  echo($sql);
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+echo('<table border="1">');
+    echo('<th>Suma lat</th>');
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo("<td>".$row['Suma']."</td>");
+        echo('</tr>');
+    }
+    echo('</table>');
+  
+    echo("<br>Średnia lat pracowników w poszczególnych działach<br>");
+$sql ="select *,AVG(YEAR(CURDATE()) - YEAR(data_urodzenia)) as srednia from pracownicy,organizacja where id_org=dzial group by dzial";
+  echo($sql);
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+echo('<table border="1">');
+    echo('<th>Dział</th><th>Średnia lat</th>');
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo("<td>".$row['dzial']."</td><td>".$row['srednia']."</td>");  
+        echo('</tr>');
+    }
+    echo('</table>');
+  
+    echo("<br>Suma lat pracowników w poszczególnych działach<br>");
+    $sql ="select *,SUM(YEAR(CURDATE()) - YEAR(data_urodzenia)) as suma from pracownicy,organizacja where id_org=dzial group by dzial"; 
+  echo($sql);
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+echo('<table border="1">');
+    echo('<th>Dział</th><th>Suma</th>');
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo("<td>".$row['dzial']."</td><td>".$row['suma']."</td>");  
+        echo('</tr>');
+    }
+    echo('</table>');
+  
+    echo("<br>Najstarsi pracownicy w każdym dziale<br>");
+    $sql ="select *,MAX(YEAR(CURDATE()) - YEAR(data_urodzenia)) as wiek from pracownicy,organizacja where id_org=dzial group by dzial"; 
+  echo($sql);
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+echo('<table border="1">');
+    echo('<th>Imię</th><th>Wiek</th><th>Nazwa Działu</th>');
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo("<td>".$row['imie']."</td><td>".$row['wiek']."</td><td>".$row['dzial']."</td>");     
         echo('</tr>');
     }
     echo('</table>');
