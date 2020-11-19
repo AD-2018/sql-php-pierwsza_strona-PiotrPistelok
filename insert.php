bbbcb344a869f3444bee2acb475d24fa88392597
PiotrPistelok <63048870+PiotrPistelok@users.noreply.github.com>
2020-11-19 14:28:16 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO Pracownik (null, imie, dzial, zarobki) 
+$sql = "INSERT INTO Pracownik (null, $_POST['name'], dzial, zarobki) 
        VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
 
 if ($conn->query($sql) === TRUE) {
