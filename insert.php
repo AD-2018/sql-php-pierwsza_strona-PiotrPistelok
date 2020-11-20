13deeec4a307a6659d2a584853e8b1001419004e
PiotrPistelok <63048870+PiotrPistelok@users.noreply.github.com>
2020-11-20 12:14:36 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,6 +1,7 @@
+
 <?php
-echo ("jestes w insert.php");
-echo $_POST['imie'];
+echo("jestes w insert.php");
+echo $_POST['name'];
 
 $servername = "remotemysql.com";
 $username = "i1g6dVaV2H";
@@ -12,15 +13,16 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO Pracownik (null, imie, dzial, zarobki, data_urodzenia) 
-       VALUES (null,'"$_POST['imie']"', 3, 20,'1999-11-21')";
-
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
+       VALUES (null,'".$_POST['imie']."', 1, 80,'1992-11-21')";
+echo ("ok");
+echo $sql;
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
-//informacja o ewentualnych błędach
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
 ?>
+
