052b65744a012f2dec36f8fe48d5da766b2b3820
PiotrPistelok <63048870+PiotrPistelok@users.noreply.github.com>
2020-11-20 11:59:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <a href="OrgPracownicy.php">Organizacja Pracownicy</a>
     <a href="agreg.php">Funkcje Agregujące</a>
     <a href="Data_czas.php">Data i czas</a>
-    <a href="formularze.html">Formularze</a>
+    <a href="danedobazy.html">Dane do bazy</a>
 </div>
 </body>
 </html>
