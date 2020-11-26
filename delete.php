36cbaeca2924716ff136db1623f9213ac2653ded
PiotrPistelok <63048870+PiotrPistelok@users.noreply.github.com>
2020-11-26 14:28:30 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -17,7 +17,7 @@ echo ("<br>");
 echo $sql;
 
 if ($conn->query($sql) === TRUE) {
-  echo "Record deleted successfully";
+  echo ("<br>"."Record deleted successfully");
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
