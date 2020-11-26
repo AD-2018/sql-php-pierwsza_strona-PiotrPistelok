03b127b3c4575de6d78a8461b9b1a51660e9f91e
PiotrPistelok <63048870+PiotrPistelok@users.noreply.github.com>
2020-11-26 14:06:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,25 @@
     <a href="Data_czas.php">Data i czas</a>
     <a href="danedobazy.html">Dane do bazy</a>
 </div>
+<php>
+require_once("connect.php");
+
+$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial";
+echo($sql);
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+echo('<table border="1">');
+    echo('<th>Id_pracownika</th><th>imię</th><th>dział</th><th>zarobki</th><th>data urodzenia</th><th>id_org</th><th>nazwa działu</th>');
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['data_urodzenia'].'</td><td>'.$row['id_org'].'</td><td>'.$row['nazwa_dzial'].'</td>');
+        echo('</tr>');
+    }
+    echo('</table>');
 </body>
 </html>
 
