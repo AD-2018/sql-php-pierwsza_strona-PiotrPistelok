a7f55512ff9e3350f51b082f3cd50751623b12f6
PiotrPistelok <63048870+PiotrPistelok@users.noreply.github.com>
2020-11-26 13:41:17 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,12 +1,18 @@
 
 <?php
-echo("jestes w insert.php");
-echo $_POST['name'];
+
+echo("jestes w insert.php <br>");
+echo "<li>". $_POST['imie'];
+echo "<li>". $_POST['dzial'];
+echo "<li>". $_POST['zarobki'];
+echo "<li>". $_POST['data_urodzenia'];
 
 require_once("connect.php");
 
+
 $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
-       VALUES (null,'".$_POST['imie']."', 1, 80,'1992-11-21')";
+       VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
+
 echo ("ok");
 
 echo $sql;
