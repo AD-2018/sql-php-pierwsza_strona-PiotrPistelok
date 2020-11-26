ec72a16297f20c21355d8ae85aa73b1c32be01f0
PiotrPistelok <63048870+PiotrPistelok@users.noreply.github.com>
2020-11-26 14:34:32 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -19,8 +19,6 @@ require_once("connect.php");
 $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
        VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
 echo ("<br>");
-echo ("ok");
-echo ("<br>");
 
 echo $sql;
 if ($conn->query($sql) === TRUE) {
