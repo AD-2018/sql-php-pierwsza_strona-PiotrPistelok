1e4c09193496d721d095ceea1b2a008a74c1a155
PiotrPistelok <pistelok.piotr@gmail.com>
2021-03-22 09:34:43 +0100

    .

--- index.php
+++ index.php
@@ -21,8 +21,8 @@
         <a class="link" href="pracownicy/danedobazy.php">Dane do bazy</a>
         <a class="link" href="biblioteka/ksiazki.php">Książki</a>
         <a class="link" href="/flexbox/index.html">Flexbox</a>
-        <a class="link" href="CSSGRID/index.html">GRID</a>
     </div>
+    <a class="link" href="CSSGRID/index.html">GRID</a>
     <div class="strona">
 <?php
     require_once("connect.php");
