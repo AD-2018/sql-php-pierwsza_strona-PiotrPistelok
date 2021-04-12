c2e29d6565e3840d31f99d7640ce7fa717bfe329
PiotrPistelok <pistelok.piotr@gmail.com>
2021-04-12 12:59:31 +0200

    .

---
+++ CSSGRID/1/index1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="style1.css">
+    <title>CSS Grid</title>
+  </head>
+  <body>
+    <div class="container">
+        <header>
+        <?php
+        require_once("connect.php");
+        $sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial";
+        echo("<br>");
+        echo($sql);
+        $result = mysqli_query($conn, $sql);
+        if ( $result) {
+            echo "<li>ok";
+            } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+        echo('<table border="1">');
+            echo('<th>Id_pracownika</th><th>imię</th><th>dział</th><th>zarobki</th><th>data urodzenia</th><th>id_org</th><th>nazwa działu</th>');
+            while($row=mysqli_fetch_assoc($result)){
+                echo('<tr>');
+                echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['data_urodzenia'].'</td><td>'.$row['id_org'].'</td><td>'.$row['nazwa_dzial'].'</td>');
+                echo('</tr>');
+        }
+            echo('</table>');
+        ?>
+        
+      </header>
+      <nav>
+      2
+      </nav>
+      <main>
+      3
+      </main>
+      <footer>
+      4
+      </footer>
+      <aside>
+      5
+      </aside>
+    </div>
+  </body>
+</html>
