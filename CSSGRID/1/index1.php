bbd58ba707d6e468f8389353c4fb00fe12067512
PiotrPistelok <pistelok.piotr@gmail.com>
2021-04-13 13:58:14 +0200

    .

--- CSSGRID/1/index1.php
+++ CSSGRID/1/index1.php
@@ -10,7 +10,26 @@
   <body>
     <div class="container">
       <header>
-      1
+      <?php
+    require_once("../../connect.php");
+    $sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>Id_pracownika</th><th>imię</th><th>dział</th><th>zarobki</th><th>data urodzenia</th><th>id_org</th><th>nazwa działu</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['data_urodzenia'].'</td><td>'.$row['id_org'].'</td><td>'.$row['nazwa_dzial'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
+?>
       </header>
       <nav>
       2
