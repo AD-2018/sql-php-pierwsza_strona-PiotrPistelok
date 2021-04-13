fa94a90475d2631477181279ad12773c4c0969db
PiotrPistelok <pistelok.piotr@gmail.com>
2021-04-13 14:01:48 +0200

    .

--- CSSGRID/1/index1.php
+++ CSSGRID/1/index1.php
@@ -12,7 +12,7 @@
       <header>
       <?php
     require_once("../../connect.php");
-    $sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial";
+    $sql = "SELECT * FROM Firma_pracownik";
     echo("<br>");
     echo($sql);
     $result = mysqli_query($conn, $sql);
@@ -22,10 +22,10 @@
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
      }
     echo('<table border="1">');
-        echo('<th>Id_pracownika</th><th>imię</th><th>dział</th><th>zarobki</th><th>data urodzenia</th><th>id_org</th><th>nazwa działu</th>');
+        echo('<th>Id_pracownika</th><th>imię</th>');
         while($row=mysqli_fetch_assoc($result)){
             echo('<tr>');
-            echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['data_urodzenia'].'</td><td>'.$row['id_org'].'</td><td>'.$row['nazwa_dzial'].'</td>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['pracownik'].'</td>');
             echo('</tr>');
      }
         echo('</table>');
