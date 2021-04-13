195b6d2d0453bb947c6136ea28e4bb65664406b8
PiotrPistelok <pistelok.piotr@gmail.com>
2021-04-13 19:55:21 +0200

    .

--- CSSGRID/3/index3.php
+++ CSSGRID/3/index3.php
@@ -10,19 +10,76 @@
   <body>
     <div class="container">
       <header>
-      1
+      <?php
+    require_once("../../connect.php");
+    $sql = "SELECT * FROM Sklep_artykuly";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>Id</th><th>Artykul</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['artykul'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
+?>
       </header>
       <nav>
       2
       </nav>
       <main>
-      3
+      <?php
+    require_once("../../connect.php");
+    $sql = "SELECT id_sklep,artykul,producent FROM Sklep_artykuly,Sklep_producenci,Sklep where Sklep_artykuly.id=Sklep.sklep_artykuly_id and Sklep_producenci.id=Sklep.sklep_producenci_id";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>Id</th><th>Artykul</th><th>Producent</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['artykul'].'</td><td>'.$row['producent'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
+?>
       </main>
       <footer>
       4
       </footer>
       <aside>
-      5
+      <?php
+    require_once("../../connect.php");
+    $sql = "SELECT * FROM Sklep_producenci";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>Id</th><th>Producent</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['producent'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
+?>
       </aside>
     </div>
   </body>
