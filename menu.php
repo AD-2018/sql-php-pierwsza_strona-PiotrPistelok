1194ec3e4e735ff2aae9b6a267bc2c1026b24650
PiotrPistelok <pistelok.piotr@gmail.com>
2021-04-19 09:42:57 +0200

    .

--- menu.php
+++ menu.php
@@ -1,10 +1,10 @@
 <ul>
-  <li class="link"> <a href="pracownicy/Pracownicy.php">Pracownicy</a></li>
-  <li class="link"> <a href="pracownicy/OrgPracownicy.php">Organizacja Pracownicy</a></li>
-  <li class="link"> <a href="pracownicy/agreg.php">Funkcje Agregujące</a></li>
-  <li class="link"> <a href="pracownicy/Data_czas.php">Data i czas</a></li>
-  <li class="link"> <a href="pracownicy/danedobazy.php">Dane do bazy</a></li>
-  <li class="link"> <a href="biblioteka/ksiazki.php">Książki</a></li>
-  <li class="link"> <a href="/flexbox/index.html">Flexbox</a></li>
-  <li class="link"> <a href="CSSGRID/index.html">GRID</a></li>
+  <li> <a class="link" href="pracownicy/Pracownicy.php">Pracownicy</a></li>
+  <li> <a class="link" href="pracownicy/OrgPracownicy.php">Organizacja Pracownicy</a></li>
+  <li> <a class="link" href="pracownicy/agreg.php">Funkcje Agregujące</a></li>
+  <li> <a class="link" href="pracownicy/Data_czas.php">Data i czas</a></li>
+  <li> <a class="link" href="pracownicy/danedobazy.php">Dane do bazy</a></li>
+  <li> <a class="link" href="biblioteka/ksiazki.php">Książki</a></li>
+  <li> <a class="link" href="/flexbox/index.html">Flexbox</a></li>
+  <li> <a class="link" href="CSSGRID/index.html">GRID</a></li>
 </ul>
