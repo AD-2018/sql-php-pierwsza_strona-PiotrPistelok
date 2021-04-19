fd5c9e84a08bea4cfd36a0e9f8db47b75979222f
PiotrPistelok <pistelok.piotr@gmail.com>
2021-04-19 09:57:54 +0200

    .

--- menu2.php
+++ menu2.php
@@ -1,6 +1,8 @@
-        <a class="link" href="../index.php">Strona główna</a>
-        <a class="link" href="OrgPracownicy.php">Organizacja Pracownicy</a>
-        <a class="link" href="Pracownicy.php">Pracownicy</a>
-        <a class="link" href="Data_czas.php">Data i czas</a>
-        <a class="link" href="agreg.php">Funkcje Agregujące</a>
-        <a class="link" href="/flexbox/index.html">Flexbox </a>
+<ul class="nav1">
+  <li class="nav2"> <a class="link" href="../index.php">Strona główna</a></li>
+  <li class="nav2"> <a class="link" href="OrgPracownicy.php">Organizacja Pracownicy</a></li>
+  <li class="nav2"> <a class="link" href="Pracownicy.php">Pracownicy</a></li>
+  <li class="nav2"> <a class="link" href="Data_czas.php">Data i czas</a></li>
+  <li class="nav2"> <a class="link" href="agreg.php">Funkcje Agregujące</a></li>
+  <li class="nav2"> <a class="link" href="/flexbox/index.html">Flexbox</a></li>
+</ul>
