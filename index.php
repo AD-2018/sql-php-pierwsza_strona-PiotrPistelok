a98037b6423304b5fb630f14b37aaba404d75298
PiotrPistelok <pistelok.piotr@gmail.com>
2021-04-19 13:06:43 +0200

    .

--- index.php
+++ index.php
@@ -6,9 +6,11 @@
 <title>Piotr Pistelok</title>
 </head>
 <body>
-    <div class="tytul">
-        <a class="link" href="https://github.com/AD-2018/sql-php-pierwsza_strona-PiotrPistelok">GitHub</a>
-        <h2>Piotr Pistelok nr 9</h2>
+    <div class="container">
+        <div class="tytul">
+            <a class="link" href="https://github.com/AD-2018/sql-php-pierwsza_strona-PiotrPistelok">GitHub</a>
+            <h2>Piotr Pistelok nr 9</h2>
+        </div>
     </div>
     <div class="nav">
         <?php include_once("menu.php");?>
