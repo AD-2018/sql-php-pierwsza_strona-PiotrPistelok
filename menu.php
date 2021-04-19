69b260317347a4b98b9fcf3032309f895e0787c5
PiotrPistelok <pistelok.piotr@gmail.com>
2021-04-19 10:20:04 +0200

    .

--- menu.php
+++ menu.php
@@ -1,10 +1,10 @@
 <ul class="nav1">
-  <li class="nav2"> <a class="link" href="pracownicy/Pracownicy.php">Pracownicy</a></li>
-  <li class="nav2"> <a class="link" href="pracownicy/OrgPracownicy.php">Organizacja Pracownicy</a></li>
-  <li class="nav2"> <a class="link" href="pracownicy/agreg.php">Funkcje Agregujące</a></li>
-  <li class="nav2"> <a class="link" href="pracownicy/Data_czas.php">Data i czas</a></li>
-  <li class="nav2"> <a class="link" href="pracownicy/danedobazy.php">Dane do bazy</a></li>
-  <li class="nav2"> <a class="link" href="biblioteka/ksiazki.php">Książki</a></li>
-  <li class="nav2"> <a class="link" href="/flexbox/index.html">Flexbox</a></li>
-  <li class="nav2"> <a class="link" href="CSSGRID/index.html">GRID</a></li>
+  <li class="nav1_link"> <a class="link" href="pracownicy/Pracownicy.php">Pracownicy</a></li>
+  <li class="nav1_link"> <a class="link" href="pracownicy/OrgPracownicy.php">Organizacja Pracownicy</a></li>
+  <li class="nav1_link"> <a class="link" href="pracownicy/agreg.php">Funkcje Agregujące</a></li>
+  <li class="nav1_link"> <a class="link" href="pracownicy/Data_czas.php">Data i czas</a></li>
+  <li class="nav1_link"> <a class="link" href="pracownicy/danedobazy.php">Dane do bazy</a></li>
+  <li class="nav1_link"> <a class="link" href="biblioteka/ksiazki.php">Książki</a></li>
+  <li class="nav1_link"> <a class="link" href="/flexbox/index.html">Flexbox</a></li>
+  <li class="nav1_link"> <a class="link" href="CSSGRID/index.html">GRID</a></li>
 </ul>
