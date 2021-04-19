087faf8997dc00fd12af4a1670d35c265fc2e586
PiotrPistelok <pistelok.piotr@gmail.com>
2021-04-19 09:31:34 +0200

    .

--- menu.php
+++ menu.php
@@ -1,8 +1,10 @@
-    <a class="link" href="pracownicy/Pracownicy.php">Pracownicy</a>
-    <a class="link" href="pracownicy/OrgPracownicy.php">Organizacja Pracownicy</a>
-    <a class="link" href="pracownicy/agreg.php">Funkcje Agregujące</a>
-    <a class="link" href="pracownicy/Data_czas.php">Data i czas</a>
-    <a class="link" href="pracownicy/danedobazy.php">Dane do bazy</a>
-    <a class="link" href="biblioteka/ksiazki.php">Książki</a>
-    <a class="link" href="/flexbox/index.html">Flexbox</a>
-    <a class="link" href="CSSGRID/index.html">GRID</a>
+<ul>
+  <li class="link" href="pracownicy/Pracownicy.php">Pracownicy</li>
+  <li class="link" href="pracownicy/OrgPracownicy.php">Organizacja Pracownicy</li>
+  <li class="link" href="pracownicy/agreg.php">Funkcje Agregujące</li>
+  <li class="link" href="pracownicy/Data_czas.php">Data i czas</li>
+  <li class="link" href="pracownicy/danedobazy.php">Dane do bazy</li>
+  <li class="link" href="biblioteka/ksiazki.php">Książki</li>
+  <li class="link" href="/flexbox/index.html">Flexbox</li>
+  <li class="link" href="CSSGRID/index.html">GRID</li>
+</ul>
