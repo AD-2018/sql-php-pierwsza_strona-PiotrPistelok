4c64d41d66dd5d249a648191d1687b7495ec1bb9
PiotrPistelok <pistelok.piotr@gmail.com>
2021-04-19 13:04:05 +0200

    poprawki

--- pracownicy/Data_czas.php
+++ pracownicy/Data_czas.php
@@ -8,7 +8,6 @@
     <div class="container">
         <div class="tytul">
             <a class="link" href="https://github.com/AD-2018/sql-php-pierwsza_strona-PiotrPistelok">GitHub</a>
-            <div class="imie">
             <h2>Piotr Pistelok nr 9</h2>
         </div>
     </div>
